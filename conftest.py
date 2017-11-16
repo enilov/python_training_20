2d27e76aa1d3d4c882e3f4f16737cf38f8a622cd
enilov <33431007+enilov@users.noreply.github.com>
2017-11-16 20:49:14 +0300

    Задание17
    загрузка информации о тестируемом приложении из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -2,22 +2,23 @@
 
 import pytest
 from fixture.application import Application
+import json
+import os.path
 
 fixture = None
-
+target = None
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    baseurl = request.config.getoption("--baseurl")
-    if fixture is None:
-        fixture = Application(browser=browser, baseurl = baseurl)
-        # fixture.session.login(username="admin", password="secret")
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, baseurl = baseurl)
-            # fixture.session.login(username="admin", password="secret")
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, baseurl=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -31,4 +32,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
